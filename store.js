0c3c6570179226702b3984b7b0b53afddd624da1
AudieRaymos <audriannakraymos@gmail.com>
2020-08-29 12:15:08 -0700

    Total + quantity js edit

--- store.js
+++ store.js
@@ -11,7 +11,15 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
 function updateCartTotal () {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
     for (var i = 0; i < cartRows.length; i++) {
         var cartRows =cartRows[i]
         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantiyElement = cartRow.getElementsByClassName('cart-quantity-input')
+        [0]
+        var price = parseFloat(priceElement.innerText.replace('$',''))
+        var quantity = quantiyElement.value
+        console.log(price = quantity)
+}
+
 }
